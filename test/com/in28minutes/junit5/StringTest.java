2a0c14627b863d12b40f21c649f037444e38db69
Christopher Sanchez <sanchez.christopherm@gmail.com>
2018-07-25 15:02:03 +0200

    Added print and print names for each test

--- test/com/in28minutes/junit5/StringTest.java
+++ test/com/in28minutes/junit5/StringTest.java
@@ -5,17 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 class StringTest {
 
 	@BeforeEach
-	void beforeEach() {
-	System.out.println("Initialize Test Data for each test");	
+	void beforeEach(TestInfo info) {
+	System.out.println("Initialize Test Data for " + info.getDisplayName());	
 	}
 	
 	@AfterEach
-	void afterEach() {
-	System.out.println("Clean up test data");	
+	void afterEach(TestInfo info) {
+	System.out.println("Clean up test data for " + info.getDisplayName());	
 	}
 	
 	@Test
