186f56614491f9f99f70f67d52fae529993c3544
Christopher Sanchez <sanchez.christopherm@gmail.com>
2018-07-25 15:08:20 +0200

    Added BeforeAll and AfterAll method

--- test/com/in28minutes/junit5/StringTest.java
+++ test/com/in28minutes/junit5/StringTest.java
@@ -2,21 +2,33 @@ package com.in28minutes.junit5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 class StringTest {
+	
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("Initialize connection to database");
+	}
+	
+	@AfterAll
+	static void afterAll() {
+		System.out.println("Close connection to database");
+	}
 
 	@BeforeEach
 	void beforeEach(TestInfo info) {
-	System.out.println("Initialize Test Data for " + info.getDisplayName());	
+		System.out.println("Initialize Test Data for " + info.getDisplayName());	
 	}
 	
 	@AfterEach
 	void afterEach(TestInfo info) {
-	System.out.println("Clean up test data for " + info.getDisplayName());	
+		System.out.println("Clean up test data for " + info.getDisplayName());	
 	}
 	
 	@Test
